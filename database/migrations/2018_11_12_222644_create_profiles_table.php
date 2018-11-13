8695679383d8873967300b35e477698ab5eb4d92
Sheik <sf354@njit.edu>
2018-11-13 16:33:59 -0500

    Created User Table Seeder

--- database/migrations/2018_11_12_222644_create_profiles_table.php
+++ database/migrations/2018_11_12_222644_create_profiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProcfilesTable extends Migration
+class CreateProfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateProcfilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('procfiles', function (Blueprint $table) {
+        Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id');
-            $table->string('fname')->nullable();
-            $table->string('lname')->nullable();
-            $table->longText('body')->nullable();
             $table->timestamps();
         });
     }
@@ -30,6 +26,6 @@ class CreateProcfilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('procfiles');
+        Schema::dropIfExists('profiles');
     }
 }
