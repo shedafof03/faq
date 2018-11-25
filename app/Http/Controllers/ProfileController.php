fa932282cf8f125e25957f9548c819722cf45064
Sheik <sf354@njit.edu>
2018-11-25 14:17:59 -0500

    Updated Profile.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use App\profile;
+use App\Profile;
 use App\user;
 
 class ProfileController extends Controller
